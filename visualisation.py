f847e3c26013d52634a9e7dcd3576c1f79025782
Jerome Kelleher <jk@well.ox.ac.uk>
2017-07-06 12:49:42 +0100

    Updated raster viz to include tree sequence.

--- visualisation.py
+++ visualisation.py
@@ -9,9 +9,9 @@ import PIL.Image as Image
 import PIL.ImageDraw as ImageDraw
 import PIL.ImageColor as ImageColor
 
-script_path = __file__ if "__file__" in locals() else "./dummy.py"
-sys.path.insert(1,os.path.join(os.path.dirname(os.path.abspath(script_path)),'..','msprime')) # use the local copy of msprime in preference to the global one
-sys.path.insert(1,os.path.join(os.path.dirname(os.path.abspath(script_path)),'..','tsinfer')) # use the local copy of tsinfer in preference to the global one
+# script_path = __file__ if "__file__" in locals() else "./dummy.py"
+# sys.path.insert(1,os.path.join(os.path.dirname(os.path.abspath(script_path)),'..','msprime')) # use the local copy of msprime in preference to the global one
+# sys.path.insert(1,os.path.join(os.path.dirname(os.path.abspath(script_path)),'..','tsinfer')) # use the local copy of tsinfer in preference to the global one
 
 import tsinfer
 import msprime
@@ -19,12 +19,29 @@ import msprime
 
 class Visualiser(object):
 
-    def __init__(self, store, samples, box_size=8):
+    def __init__(self, store, samples, inferred_ts, box_size=8):
         self.store = store
         self.box_size = box_size
         self.samples = samples
+        self.inferred_ts = inferred_ts
+
+        self.top_padding = box_size
+        self.left_padding = box_size
+        self.bottom_padding = box_size
+        self.mid_padding = 2 * box_size
+        self.right_padding = box_size
+        self.background_colour = ImageColor.getrgb("white")
+        self.copying_outline_colour = ImageColor.getrgb("black")
+        self.colours = {
+            0: ImageColor.getrgb("blue"),
+            1: ImageColor.getrgb("red")}
+        self.copy_colours = {
+            0: ImageColor.getrgb("black"),
+            1: ImageColor.getrgb("green")}
+
+        # Make the haplotype box
         frequency = np.sum(samples, axis=0)
-        num_rows = 1
+        num_haplotype_rows = 1
         self.row_map = {0:0}
         last_frequency = 0
         self.ancestors = np.zeros((store.num_ancestors, store.num_sites), dtype=np.int8)
@@ -33,107 +50,142 @@ class Visualiser(object):
                     j, self.ancestors[j, :])
             if frequency[focal_sites[0]] != last_frequency:
                 last_frequency = frequency[focal_sites[0]]
-                num_rows += 1
-            self.row_map[j] = num_rows
-            num_rows += 1
-        num_rows += 1
+                num_haplotype_rows += 1
+            self.row_map[j] = num_haplotype_rows
+            num_haplotype_rows += 1
+        num_ancestor_rows = num_haplotype_rows
+        num_haplotype_rows += 1
         for j in range(samples.shape[0]):
-            self.row_map[store.num_ancestors + j] = num_rows
-            num_rows += 1
+            self.row_map[store.num_ancestors + j] = num_haplotype_rows
+            num_haplotype_rows += 1
 
-        self.top_padding = box_size
-        self.left_padding = box_size
-        self.bottom_padding = box_size
-        self.right_padding = box_size
-        self.background_colour = ImageColor.getrgb("white")
-        self.copying_outline_colour = ImageColor.getrgb("black")
-        self.copying_opacity = 64
-        self.colours = {
-            0: ImageColor.getrgb("blue"),
-            1: ImageColor.getrgb("red")}
+        # Make the tree sequence box.
+        num_ts_rows = num_ancestor_rows
+
         self.width = box_size * store.num_sites + self.left_padding + self.right_padding
-        self.height = box_size * num_rows + self.top_padding + self.bottom_padding
-        self.base_image = Image.new("RGBA", (self.width, self.height))
+        self.height = (
+            self.top_padding + self.bottom_padding + self.mid_padding
+            + num_haplotype_rows * box_size + num_ts_rows * box_size)
+        self.ts_origin = (self.left_padding, self.top_padding)
+        self.haplotype_origin = (
+                self.left_padding,
+                self.top_padding + self.mid_padding + num_ts_rows * box_size)
+        self.base_image = Image.new(
+            "RGB", (self.width, self.height), color=self.background_colour)
         self.draw_base()
 
     def draw_base(self):
-        # Draw a white background over everything.
+        self.draw_base_tree_sequence()
+        self.draw_base_haplotypes()
+
+    def draw_base_tree_sequence(self):
+        draw = ImageDraw.Draw(self.base_image)
+        I = np.zeros((self.store.num_ancestors, self.store.num_sites))
+        self.__draw_ts_intensity(draw, I)
+
+    def draw_base_haplotypes(self):
         b = self.box_size
         draw = ImageDraw.Draw(self.base_image)
-        draw.rectangle([(0, 0), (self.width, self.height)], fill=self.background_colour)
+        origin = self.haplotype_origin
         # Draw the ancestors
         for j in range(self.ancestors.shape[0]):
             a = self.ancestors[j]
             row = self.row_map[j]
-            y = row * b + self.top_padding
+            y = row * b + origin[1]
             for k in range(self.store.num_sites):
-                x = k * b + self.left_padding
+                x = k * b + origin[0]
                 if a[k] != -1:
                     draw.rectangle([(x, y), (x + b, y + b)], fill=self.colours[a[k]])
         # Draw the samples
         for j in range(self.samples.shape[0]):
             a = self.samples[j]
             row = self.row_map[self.store.num_ancestors + j]
-            y = row * b + self.top_padding
+            y = row * b + origin[1]
             for k in range(self.store.num_sites):
-                x = k * b + self.left_padding
+                x = k * b + origin[0]
                 draw.rectangle([(x, y), (x + b, y + b)], fill=self.colours[a[k]])
 
     def draw_haplotypes(self, filename):
         self.base_image.save(filename)
 
-    def draw_copying_path(self, filename, child_row, parents):
-        # print("copying path", filename, child_row, parents)
+    def __draw_ts_intensity(self, draw, child_intensity):
+        b = self.box_size
+        origin = self.ts_origin
+        for j in range(self.ancestors.shape[0]):
+            a = self.ancestors[j]
+            row = self.row_map[j]
+            y = row * b + origin[1]
+            for k in range(self.store.num_sites):
+                x = k * b + origin[0]
+                hsl = "hsl(240, {}%, 50%)".format(int(child_intensity[j, k] * 100))
+                fill = ImageColor.getrgb(hsl)
+                draw.rectangle(
+                    [(x, y), (x + b, y + b)], fill=fill, outline="black")
+
+
+    def draw_copying_path(self, filename, child_row, parents, child_intensity):
+        origin = self.haplotype_origin
         b = self.box_size
         m = self.store.num_sites
         image = self.base_image.copy()
         draw = ImageDraw.Draw(image)
-        y = self.row_map[child_row] * b + self.top_padding
-        x = self.left_padding
+        y = self.row_map[child_row] * b + origin[1]
+        x = origin[0]
         draw.rectangle([(x, y), (x + m * b, y + b)], outline=self.copying_outline_colour)
         for k in range(m):
             row = self.row_map[parents[k]]
-            y = row * b + self.top_padding
-            x = k * b + self.left_padding
+            y = row * b + origin[1]
+            x = k * b + origin[0]
             a = self.ancestors[parents[k], k]
-            # fill = tuple(list(self.colours[a]) + [self.copying_opacity])
-            fill = "black"
-            draw.rectangle([(x, y), (x + b, y + b)], fill=fill)
+            draw.rectangle([(x, y), (x + b, y + b)], fill=self.copy_colours[a])
         print("Saving", filename)
+        self.__draw_ts_intensity(draw, child_intensity)
         image.save(filename)
 
-    def draw_copying_paths(self, tree_sequence, pattern):
+    def draw_copying_paths(self, pattern):
         N = self.store.num_ancestors + self.samples.shape[0]
         P = np.zeros((N, self.store.num_sites), dtype=int) - 1
+        C = np.zeros((self.store.num_ancestors, self.store.num_sites), dtype=int)
+        ts = self.inferred_ts
         site_index = {}
-        for site in tree_sequence.sites():
+        for site in ts.sites():
             site_index[site.position] = site.index
-        site_index[tree_sequence.sequence_length] = tree_sequence.num_sites
+        site_index[ts.sequence_length] = ts.num_sites
         site_index[0] = 0
-        for e in tree_sequence.edgesets():
+        max_num_children = 0
+        for e in ts.edgesets():
+            if e.parent != 0:
+                max_num_children = max(max_num_children, len(e.children))
             for c in e.children:
                 left = site_index[e.left]
                 right = site_index[e.right]
                 assert left < right
                 P[c, left:right] = e.parent
+        index = np.arange(self.store.num_sites, dtype=int)
         for j in range(1, self.store.num_ancestors):
-            self.draw_copying_path(pattern.format(j), j, P[j])
+            C[P[j],index] += 1
+            I = C / max_num_children
+            self.draw_copying_path(pattern.format(j), j, P[j], I)
         for j in range(self.samples.shape[0]):
             k = self.store.num_ancestors + j
-            self.draw_copying_path(pattern.format(k), k, P[k])
+            C[P[k], index] += 1
+            I = C / max_num_children
+            self.draw_copying_path(pattern.format(k), k, P[k], I)
 
 
 def visualise(
         samples, positions, length, recombination_rate, error_rate, method="C",
         box_size=8):
-
     store = tsinfer.build_ancestors(samples, positions, method=method)
     inferred_ts = tsinfer.infer(
         samples, positions, length, recombination_rate, error_rate, method=method)
-    visualiser = Visualiser(store, samples, box_size=box_size)
+    simplified_ts = inferred_ts.simplify()
+    for e in simplified_ts.edgesets():
+        print("{:.2f}\t{:.2f}".format(e.left, e.right), e.parent, e.children, sep="\t")
+    visualiser = Visualiser(store, samples, inferred_ts, box_size=box_size)
     prefix = "tmp__NOBACKUP__/"
     visualiser.draw_haplotypes(os.path.join(prefix, "haplotypes.png"))
-    visualiser.draw_copying_paths(inferred_ts, os.path.join(prefix, "copying_{}.png"))
+    visualiser.draw_copying_paths(os.path.join(prefix, "copying_{}.png"))
 
 
 def run_viz(n, L, seed):
@@ -151,7 +203,7 @@ def run_viz(n, L, seed):
 
 
 def main():
-    run_viz(15, 20, 1)
+    run_viz(10, 10, 1)
 
 
 if __name__ == "__main__":
