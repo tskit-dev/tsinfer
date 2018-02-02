ebebcf33522145a5b471fb9baf8de8b46d038a6c
Yan Wong <yan@yanwong.me>
2018-02-02 09:49:51 +0000

    allow multiple files & progress

--- run_tsinf.py
+++ run_tsinf.py
@@ -10,22 +10,36 @@ import bsddb3
 import tsinfer
 
 parser = argparse.ArgumentParser(description=description)
-parser.add_argument('infile', 
-                    help='a .tsinf file produced e.g. by vcf2tsinfer.py')
+parser.add_argument('infiles', nargs="+",
+                    help='One or more .tsinf files produced e.g. by vcf2tsinfer.py.')
 parser.add_argument('-o', '--outfile',
-                    help='the output file. If not given, will save under same name as infile with a .ts extension')
+                    help='The output file name. If not given, will save under same name as infile with a .ts extension' \
+                    'if more than one input file, will append ".0", ".1", ".2" etc to the name.')
+parser.add_argument('-P', '--progress', action='store_true',
+                    help='Show a progress bar.')
 args = parser.parse_args()
 
-method="C"
-input_hdf5 = zarr.DBMStore(args.infile, open=bsddb3.btopen)
-input_root = zarr.group(store=input_hdf5)
+method, path_compression, simplify = "C", True, True #set defaults
 
-ancestors_root = zarr.group()
-tsinfer.build_ancestors(
-    input_root, ancestors_root, method=method, chunk_size=16, compress=False)
-ancestors_ts = tsinfer.match_ancestors(
-    input_root, ancestors_root, method=method, path_compression=path_compression)
-full_inferred_ts = tsinfer.match_samples(
-    input_root, ancestors_ts, method=method, path_compression=path_compression,
-    simplify=simplify)
-full_inferred_ts.dump(args.outfile or (os.path.splitext(infile) + '.ts'))
+for i, fn in enumerate(args.infiles):
+    ext = ('.' + str(i)) if len(args.infiles) > 1 else ''
+    if args.outfile:
+        out_fn = args.outfile + ext
+    else:
+        out_fn = os.path.splitext(fn)[0] + '.ts'
+    if not os.path.isfile(fn):
+        raise FileNotFoundError
+    input_hdf5 = zarr.DBMStore(fn, open=bsddb3.btopen)
+    input_root = zarr.group(store=input_hdf5)
+    
+    ancestors_root = zarr.group()
+    tsinfer.build_ancestors(
+        input_root, ancestors_root, method=method, chunk_size=16, compress=False,
+        progress = args.progress)
+    ancestors_ts = tsinfer.match_ancestors(
+        input_root, ancestors_root, method=method, path_compression=path_compression,
+        progress = args.progress)
+    full_inferred_ts = tsinfer.match_samples(
+        input_root, ancestors_ts, method=method, path_compression=path_compression,
+        simplify=simplify, progress = args.progress)
+    full_inferred_ts.dump(out_fn)
