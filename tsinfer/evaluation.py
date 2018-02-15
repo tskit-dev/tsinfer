Fixed subtle numerical bug resulting in random recombinations.

tsinfer/evaluation.py
@@ -9,7 +9,6 @@ import numpy as np
 import msprime
 
 import tsinfer.inference as inference
-import tsinfer.formats as formats
 
 
 def kc_distance(tree1, tree2):
@@ -93,7 +92,8 @@ def strip_singletons(ts):
             for mutation in variant.site.mutations:
                 assert mutation.parent == -1  # No back mutations
                 mutations.add_row(
-                    site=site_id, node=mutation.node, derived_state=mutation.derived_state)
+                    site=site_id, node=mutation.node,
+                    derived_state=mutation.derived_state)
     tables = ts.dump_tables()
     return msprime.load_tables(
         nodes=tables.nodes, edges=tables.edges, sites=sites, mutations=mutations)
@@ -224,7 +224,6 @@ def build_simulated_ancestors(sample_data, ancestor_data, ts):
 
     ancestors, start, end, focal_sites = get_ancestor_descriptors(A)
     time = len(ancestors)
-    num_ancestors = len(ancestors)
     for a, s, e, focal in zip(ancestors, start, end, focal_sites):
         assert np.all(a[:s] == inference.UNKNOWN_ALLELE)
         assert np.all(a[s:e] != inference.UNKNOWN_ALLELE)
@@ -234,3 +233,34 @@ def build_simulated_ancestors(sample_data, ancestor_data, ts):
             start=s, end=e, time=time, focal_sites=np.array(focal, dtype=np.int32),
             haplotype=a)
         time -= 1
+
+
+def print_tree_pairs(ts1, ts2):
+    """
+    Prints out the trees at each point in the specified tree sequences,
+    alone with their KC distance.
+    """
+    for (left, right), tree1, tree2 in tree_pairs(ts1, ts2):
+        distance = kc_distance(tree1, tree2)
+        trailer = ""
+        if distance != 0:
+            trailer = "[MISMATCH]"
+        print("-" * 20)
+        print("Interval          =", left, "--", right)
+        print("Source interval   =", tree1.interval)
+        print("Inferred interval =", tree2.interval)
+        print("KC distance       =", distance, trailer)
+        print()
+        d1 = tree1.draw(format="unicode").splitlines()
+        d2 = tree2.draw(format="unicode").splitlines()
+        j = 0
+        while j < (min(len(d1), len(d2))):
+            print(d1[j], " | ", d2[j])
+            j += 1
+        while j < len(d1):
+            print(d1[j], " |")
+            j += 1
+        while j < len(d2):
+            print(" " * len(d1[0]), " | ", d2[j])
+            j += 1
+        print()

tsinfer/inference.py
@@ -144,7 +144,7 @@ def build_ancestors(
 def match_ancestors(
         sample_data, ancestor_data, output_path=None, method="C", progress=False,
         num_threads=0, path_compression=True, output_interval=None, resume=False,
-        traceback_file_pattern=None):
+        traceback_file_pattern=None, extended_checks=False):
     """
     Runs the copying process of the specified input and ancestors and returns
     the resulting tree sequence.
@@ -153,7 +153,8 @@ def match_ancestors(
         sample_data, ancestor_data, output_path=output_path, method=method,
         progress=progress, path_compression=path_compression,
         num_threads=num_threads, output_interval=output_interval,
-        resume=resume, traceback_file_pattern=traceback_file_pattern)
+        resume=resume, traceback_file_pattern=traceback_file_pattern,
+        extended_checks=extended_checks)
     return matcher.match_ancestors()
 
 
@@ -191,12 +192,13 @@ def verify(input_hdf5, ancestors_hdf5, ancestors_ts, progress=False):
 def match_samples(
         sample_data, ancestors_ts, genotype_quality=0, method="C", progress=False,
         num_threads=0, path_compression=True, simplify=True,
-        traceback_file_pattern=None):
+        traceback_file_pattern=None, extended_checks=False):
     manager = SampleMatcher(
         sample_data, ancestors_ts, error_probability=genotype_quality,
         path_compression=path_compression,
         method=method, progress=progress, num_threads=num_threads,
-        traceback_file_pattern=traceback_file_pattern)
+        traceback_file_pattern=traceback_file_pattern,
+        extended_checks=extended_checks)
     manager.match_samples()
     ts = manager.finalise(simplify=simplify)
     return ts
@@ -209,7 +211,8 @@ class Matcher(object):
 
     def __init__(
             self, sample_data, error_probability=0, num_threads=1, method="C",
-            path_compression=True, progress=False, traceback_file_pattern=None):
+            path_compression=True, progress=False, traceback_file_pattern=None,
+            extended_checks=False):
         self.sample_data = sample_data
         self.num_threads = num_threads
         self.path_compression = path_compression
@@ -219,6 +222,7 @@ class Matcher(object):
         self.positions = self.sample_data.position[:][self.sample_data.variant_site]
         self.recombination_rate = self.sample_data.recombination_rate
         self.progress = progress
+        self.extended_checks = extended_checks
         self.tree_sequence_builder_class = algorithm.TreeSequenceBuilder
         if method == "C":
             logger.debug("Using C matcher implementation")
@@ -253,7 +257,9 @@ class Matcher(object):
         self.num_matches = np.zeros(num_threads)
         logger.info("Setting match error probability to {}".format(error_probability))
         self.matcher = [
-            self.ancestor_matcher_class(self.tree_sequence_builder, error_probability)
+            self.ancestor_matcher_class(
+                self.tree_sequence_builder, error_probability,
+                extended_checks=self.extended_checks)
             for _ in range(num_threads)]
         # The progress monitor is allocated later by subclasses.
         self.progress_monitor = None