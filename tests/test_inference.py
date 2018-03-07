Worked around numerical bug & updated tests.

tests/test_inference.py
@@ -136,11 +136,12 @@ class TestMutationProperties(unittest.TestCase):
 
     def test_error(self):
         num_sites = 20
+        np.random.seed(100)
         S, positions = get_random_data_example(5, num_sites)
         for method in ["python", "c"]:
             ts = tsinfer.infer(
                 genotypes=S, positions=positions, sequence_length=num_sites,
-                recombination_rate=1e-9, sample_error=0.1, method=method)
+                recombination_rate=1e-9, sample_error=0.2, method=method)
             self.assertEqual(ts.num_sites, num_sites)
             self.assertGreater(ts.num_mutations, num_sites)
             back_mutation = False
@@ -357,7 +358,7 @@ class TestBuildAncestors(unittest.TestCase):
         # The first ancestor must be all zeros.
         self.assertEqual(start[0], 0)
         self.assertEqual(end[0], ancestor_data.num_sites)
-        self.assertEqual(time[0], 1 + max(
+        self.assertEqual(time[0], 2 + max(
             np.sum(genotypes) for genotypes in sample_genotypes))
         self.assertEqual(list(focal_sites[0]), [])
         self.assertTrue(np.all(ancestor_haplotypes[0] == 0))
@@ -517,21 +518,24 @@ class TestPartialAncestorMatching(unittest.TestCase):
         ancestor_data = tsinfer.AncestorData.initialise(sample_data)
 
         ancestor_data.add_ancestor(  # ID 0
-            start=0, end=6, focal_sites=[], time=4, haplotype=[0, 0, 0, 0, 0, 0])
+            start=0, end=6, focal_sites=[], time=5, haplotype=[0, 0, 0, 0, 0, 0])
         ancestor_data.add_ancestor(  # ID 1
-            start=0, end=3, focal_sites=[2], time=3, haplotype=[0, 0, 1, -1, -1, -1])
+            start=0, end=6, focal_sites=[], time=4, haplotype=[0, 0, 0, 0, 0, 0])
         ancestor_data.add_ancestor(  # ID 2
-            start=3, end=6, focal_sites=[4], time=2, haplotype=[-1, -1, -1, 0, 1, 0])
+            start=0, end=3, focal_sites=[2], time=3, haplotype=[0, 0, 1, -1, -1, -1])
         ancestor_data.add_ancestor(  # ID 3
+            start=3, end=6, focal_sites=[4], time=2, haplotype=[-1, -1, -1, 0, 1, 0])
+        ancestor_data.add_ancestor(  # ID 4
             start=0, end=6, focal_sites=[0, 1, 3, 5], time=1,
             haplotype=[1, 1, 1, 1, 1, 1])
         ancestor_data.finalise()
 
         expected_edges = [
-            msprime.Edge(0, 3, 1, 3),
-            msprime.Edge(3, 6, 2, 3),
-            msprime.Edge(3, 6, 0, 2),
-            msprime.Edge(0, 3, 0, 1)]
+            msprime.Edge(0, 6, 0, 1),
+            msprime.Edge(0, 3, 2, 4),
+            msprime.Edge(3, 6, 3, 4),
+            msprime.Edge(3, 6, 1, 3),
+            msprime.Edge(0, 3, 1, 2)]
         self.verify_edges(sample_data, ancestor_data, expected_edges)
 
     def test_partial_overlap(self):
@@ -543,21 +547,25 @@ class TestPartialAncestorMatching(unittest.TestCase):
         ancestor_data = tsinfer.AncestorData.initialise(sample_data)
 
         ancestor_data.add_ancestor(  # ID 0
-            start=0, end=7, focal_sites=[], time=4, haplotype=[0, 0, 0, 0, 0, 0, 0])
+            start=0, end=7, focal_sites=[], time=5, haplotype=[0, 0, 0, 0, 0, 0, 0])
         ancestor_data.add_ancestor(  # ID 1
-            start=0, end=3, focal_sites=[2], time=3, haplotype=[0, 0, 1, 0, 0, 0, 0])
+            start=0, end=7, focal_sites=[], time=4, haplotype=[0, 0, 0, 0, 0, 0, 0])
         ancestor_data.add_ancestor(  # ID 2
-            start=3, end=7, focal_sites=[4, 6], time=2, haplotype=[-1, -1, -1, 0, 1, 0, 1])
+            start=0, end=3, focal_sites=[2], time=3, haplotype=[0, 0, 1, 0, 0, 0, 0])
         ancestor_data.add_ancestor(  # ID 3
+            start=3, end=7, focal_sites=[4, 6], time=2,
+            haplotype=[-1, -1, -1, 0, 1, 0, 1])
+        ancestor_data.add_ancestor(  # ID 4
             start=0, end=7, focal_sites=[0, 1, 3, 5], time=1,
             haplotype=[1, 1, 1, 1, 1, 1, 1])
         ancestor_data.finalise()
 
         expected_edges = [
-            msprime.Edge(0, 3, 1, 3),
-            msprime.Edge(3, 7, 2, 3),
-            msprime.Edge(3, 7, 0, 2),
-            msprime.Edge(0, 3, 0, 1)]
+            msprime.Edge(0, 7, 0, 1),
+            msprime.Edge(0, 3, 2, 4),
+            msprime.Edge(3, 7, 3, 4),
+            msprime.Edge(3, 7, 1, 3),
+            msprime.Edge(0, 3, 1, 2)]
         self.verify_edges(sample_data, ancestor_data, expected_edges)
 
     def test_edge_overlap_bug(self):
@@ -569,35 +577,39 @@ class TestPartialAncestorMatching(unittest.TestCase):
         ancestor_data = tsinfer.AncestorData.initialise(sample_data)
 
         ancestor_data.add_ancestor(  # ID 0
-            start=0, end=12, focal_sites=[], time=7,
+            start=0, end=12, focal_sites=[], time=8,
             haplotype=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
         ancestor_data.add_ancestor(  # ID 1
+            start=0, end=12, focal_sites=[], time=7,
+            haplotype=[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+        ancestor_data.add_ancestor(  # ID 2
             start=0, end=4, focal_sites=[], time=6,
             haplotype=[0, 0, 0, 0, -1, -1, -1, -1, -1, -1, -1, -1])
-        ancestor_data.add_ancestor(  # ID 2
+        ancestor_data.add_ancestor(  # ID 3
             start=4, end=12, focal_sites=[], time=5,
             haplotype=[-1, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0])
-        ancestor_data.add_ancestor(  # ID 3
+        ancestor_data.add_ancestor(  # ID 4
             start=8, end=12, focal_sites=[9, 11], time=4,
             haplotype=[-1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 0, 1])
-        ancestor_data.add_ancestor(  # ID 4
+        ancestor_data.add_ancestor(  # ID 5
             start=4, end=8, focal_sites=[5, 7], time=3,
             haplotype=[-1, -1, -1, -1, 0, 1, 0, 1, -1, -1, -1, -1])
-        ancestor_data.add_ancestor(  # ID 5
+        ancestor_data.add_ancestor(  # ID 6
             start=0, end=4, focal_sites=[1, 3], time=2,
             haplotype=[0, 1, 0, 1, -1, -1, -1, -1, -1, -1, -1, -1])
-        ancestor_data.add_ancestor(  # ID 6
+        ancestor_data.add_ancestor(  # ID 7
             start=0, end=12, focal_sites=[0, 2, 4, 6, 8, 10], time=1,
             haplotype=[1, 0, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0])
         ancestor_data.finalise()
 
         expected_edges = [
-            msprime.Edge(0, 4, 0, 1),
-            msprime.Edge(4, 12, 0, 2),
-            msprime.Edge(8, 12, 0, 3),
-            msprime.Edge(4, 8, 0, 4),
-            msprime.Edge(0, 4, 0, 5),
-            msprime.Edge(0, 4, 0, 6),
-            msprime.Edge(4, 8, 4, 6),
-            msprime.Edge(8, 12, 0, 6)]
+            msprime.Edge(0, 12, 0, 1),
+            msprime.Edge(0, 4, 1, 2),
+            msprime.Edge(4, 12, 1, 3),
+            msprime.Edge(8, 12, 1, 4),
+            msprime.Edge(4, 8, 1, 5),
+            msprime.Edge(0, 4, 1, 6),
+            msprime.Edge(0, 4, 1, 7),
+            msprime.Edge(4, 8, 5, 7),
+            msprime.Edge(8, 12, 1, 7)]
         self.verify_edges(sample_data, ancestor_data, expected_edges)

tsinfer/evaluation.py
@@ -115,8 +115,10 @@ def insert_perfect_mutations(ts, delta=None):
 
     num_children = np.zeros(ts.num_nodes, dtype=int)
     parent = np.zeros(ts.num_nodes, dtype=int) - 1
-    if delta is None:
-        delta = 1 / (8 * ts.num_samples)
+
+    current_delta = 0
+    if delta is not None:
+        current_delta = delta
 
     for (left, right), edges_out, edges_in in ts.edge_diffs():
         last_parent = list(parent)
@@ -140,20 +142,24 @@ def insert_perfect_mutations(ts, delta=None):
 
         if len(edges_out) > 4:
             raise ValueError("Multiple recombination detected")
-
-        x = left - delta
+        # We use the value of delta from the previous iteration
+        x = left - current_delta
         for u in list(children_out - children_in) + list(children_in & children_out):
             if last_num_children[u] > 0:
                 site_id = tables.sites.add_row(position=x, ancestral_state="0")
                 tables.mutations.add_row(site=site_id, node=u, derived_state="1")
-                x -= delta
+                x -= current_delta
 
+        # Now update delta for this interval.
+        if delta is None:
+            max_nodes = 2 * (len(children_out) + len(children_in)) + len(parents_in) + 1
+            current_delta = (right - left) / max_nodes
         x = left
         for c in list(children_in - children_out) + list(children_in & children_out):
             if num_children[c] > 0:
                 site_id = tables.sites.add_row(position=x, ancestral_state="0")
                 tables.mutations.add_row(site=site_id, node=c, derived_state="1")
-                x += delta
+                x += current_delta
 
         # It seems wrong that we have to mark every parent, since a few of these
         # will already have been marked out by the children.
@@ -162,7 +168,7 @@ def insert_perfect_mutations(ts, delta=None):
                 # print("marking in parent", u, "at", x)
                 site_id = tables.sites.add_row(position=x, ancestral_state="0")
                 tables.mutations.add_row(site=site_id, node=u, derived_state="1")
-                x += delta
+                x += current_delta
 
     msprime.sort_tables(**tables.asdict())
     return msprime.load_tables(**tables.asdict())
@@ -224,8 +230,6 @@ def get_ancestor_descriptors(A):
             focal_sites.append(new_sites)
             start.append(s)
             end.append(e)
-        else:
-            warnings.warn("Unknown ancestor provided")
     return np.array(ancestors, dtype=np.uint8), start, end, focal_sites
 
 

tsinfer/inference.py
@@ -15,7 +15,6 @@ import numpy as np
 import tqdm
 import humanize
 import msprime
-import zarr
 
 import _tsinfer
 import tsinfer.formats as formats
@@ -26,6 +25,7 @@ logger = logging.getLogger(__name__)
 
 UNKNOWN_ALLELE = 255
 PHRED_MAX = 255
+
 
 def proba_to_phred(probability, min_value=1e-10):
     """
@@ -65,7 +65,6 @@ def phred_to_proba(phred_score):
     if scalar_input:
         return np.squeeze(ret)
     return ret
-
 
 
 def infer(
@@ -108,7 +107,7 @@ def build_ancestors(
     progress_monitor = tqdm.tqdm(total=num_sites, disable=not progress)
     frequency = input_data.frequency[:]
     logger.info("Starting site addition")
-    for j, (site_id, genotypes)  in enumerate(input_data.variants()):
+    for j, (site_id, genotypes) in enumerate(input_data.variants()):
         ancestor_builder.add_site(j, int(frequency[site_id]), genotypes)
         progress_monitor.update()
     progress_monitor.close()
@@ -117,15 +116,23 @@ def build_ancestors(
     descriptors = ancestor_builder.ancestor_descriptors()
     num_ancestors = len(descriptors) + 1
     if num_ancestors > 1:
-        ultimate_ancestor_time = descriptors[0][0] + 1
         logger.info("Starting build for {} ancestors".format(num_ancestors))
+        ultimate_ancestor_time = descriptors[0][0] + 1
         a = np.zeros(num_sites, dtype=np.uint8)
+        # FIXME quick hack to make sure that the ultimate ancestor is always
+        # defined. This should be addressed more elegantly within the matching
+        # code, as we now assume that there must be this single "meta-ancestor"
+        # for the algorithm to work.
+        ancestor_data.add_ancestor(
+            start=0, end=num_sites, time=ultimate_ancestor_time + 1,
+            focal_sites=np.array([], dtype=np.int32), haplotype=a)
         # Add the ultimate ancestor.
         ancestor_data.add_ancestor(
             start=0, end=num_sites, time=ultimate_ancestor_time,
             focal_sites=np.array([], dtype=np.int32), haplotype=a)
 
-        progress_monitor = tqdm.tqdm(total=num_ancestors, initial=1, disable=not progress)
+        progress_monitor = tqdm.tqdm(
+            total=num_ancestors, initial=1, disable=not progress)
         for freq, focal_sites in descriptors:
             before = time.perf_counter()
             # TODO: This is a read-only process so we can multithread it.
@@ -170,7 +177,6 @@ def verify(input_hdf5, ancestors_hdf5, ancestors_ts, progress=False):
         raise ValueError("Incorrect number of ancestors")
     if ancestors_ts.num_sites != input_file.num_sites:
         raise ValueError("Incorrect number of sites")
-
 
     progress_monitor = tqdm.tqdm(
         total=ancestors_ts.num_sites, disable=not progress, dynamic_ncols=True)
@@ -308,7 +314,7 @@ class Matcher(object):
         return left, right, parent
 
     def restore_tree_sequence_builder(self, ancestors_ts):
-        before = time.perf_counter()
+        # before = time.perf_counter()
         tables = ancestors_ts.dump_tables()
         nodes = tables.nodes
         self.tree_sequence_builder.restore_nodes(nodes.time, nodes.flags)
@@ -332,8 +338,8 @@ class Matcher(object):
         # print("SITE  =", self.mutated_sites)
         logger.info(
             "Loaded {} samples {} nodes; {} edges; {} sites; {} mutations".format(
-            ancestors_ts.num_samples, len(nodes), len(edges), ancestors_ts.num_sites,
-            len(mutations)))
+                ancestors_ts.num_samples, len(nodes), len(edges), ancestors_ts.num_sites,
+                len(mutations)))
 
     def get_tree_sequence(self, rescale_positions=True, all_sites=False):
         """
@@ -490,7 +496,7 @@ class AncestorMatcher(Matcher):
         assert np.all(haplotype[focal_sites] == 1)
         logger.debug(
             "Finding path for ancestor {}; start={} end={} num_focal_sites={}".format(
-            ancestor_id, start, end, focal_sites.shape[0]))
+                ancestor_id, start, end, focal_sites.shape[0]))
         assert np.all(haplotype[focal_sites] == 1)
         left, right, parent = self._find_path(
                 ancestor_id, haplotype, start, end, thread_index)
@@ -695,7 +701,6 @@ class SampleMatcher(Matcher):
         logger.info("Finalising tree sequence")
         ts = self.get_tree_sequence(all_sites=True)
         if simplify:
-            N = ts.num_nodes
             logger.info("Running simplify on {} nodes and {} edges".format(
                 ts.num_nodes, ts.num_edges))
             ts = ts.simplify(