a10f5743eb23c08e909ec7311e3e9a8a17b64322
Jerome Kelleher <jk@well.ox.ac.uk>
2018-05-01 16:32:37 +0100

    Updated 1000G conversion script.

--- convert_1kg.py
+++ convert_1kg.py
@@ -1,6 +1,5 @@
 """
-Example script for importing 1000 Genomes data using the HapMap
-genetic map.
+Example script for importing 1000 Genomes data.
 """
 import argparse
 import subprocess
@@ -15,13 +14,13 @@ import attr
 import cyvcf2
 import tqdm
 
-import zarr
-import bsddb3
 
 @attr.s()
-class Variant(object):
+class Site(object):
     position = attr.ib(None)
+    alleles = attr.ib(None)
     genotypes = attr.ib(None)
+    metadata = attr.ib({})
 
 
 def filter_duplicates(vcf):
@@ -70,65 +69,49 @@ def variants(vcf_path, show_progress=False):
             pass
         if row.num_called == num_diploids and ancestral_state is not None:
             a = np.zeros(num_samples, dtype=np.uint8)
-            if row.is_snp and len(row.ALT) == 1:
-                # Fill in a with genotypes.
-                bases = row.gt_bases
-                for j in range(num_diploids):
-                    a[2 * j] = bases[j][0] != ancestral_state
-                    a[2 * j + 1] = bases[j][2] != ancestral_state
-                yield Variant(position=row.POS, genotypes=a)
+            all_alleles = set([ancestral_state])
+            # Fill in a with genotypes.
+            bases = np.array(row.gt_bases)
+            for j in range(num_diploids):
+                alleles = bases[j].split("|")
+                for allele in alleles:
+                    all_alleles.add(allele)
+                a[2 * j] = alleles[0] != ancestral_state
+                a[2 * j + 1] = alleles[1] != ancestral_state
+            if len(all_alleles) == 2:
+                all_alleles.remove(ancestral_state)
+                alleles = [ancestral_state, all_alleles.pop()]
+                metadata = {"ID": row.ID, "INFO": dict(row.INFO)}
+                yield Site(
+                    position=row.POS, alleles=alleles, genotypes=a, metadata=metadata)
+
     vcf.close()
 
-def convert(vcf_file, genetic_map_file, output_file, max_variants=None, show_progress=False):
-    genetic_map = msprime.RecombinationMap.read_hapmap(genetic_map_file)
-    map_length = genetic_map.get_length()
+def convert(vcf_file, output_file, max_variants=None, show_progress=False):
 
     if max_variants is None:
         max_variants = 2**32  # Arbitrary, but > defined max for VCF
 
-    last_physical_pos = 0
-    last_genetic_pos = 0
-    # This is crud, need to abstract this away from the user.
-    if os.path.exists(output_file):
-        shutil.rmtree(output_file)
-
+    sample_data = tsinfer.SampleData.initialise(filename=output_file)
     vcf = cyvcf2.VCF(vcf_file)
-    num_diploids = len(vcf.samples)
-    num_samples = 2 * num_diploids
+    for sample in vcf.samples:
+        metadata = {"name": sample}
+        sample_data.add_sample(metadata)
+        sample_data.add_sample(metadata)
     vcf.close()
 
-    sample_data = tsinfer.SampleData.initialise(
-        num_samples=num_samples, sequence_length=map_length,
-        filename=output_file)
-
-    # for v in ts.variants():
-    #     sample_data.add_variant(v.site.position, v.alleles, v.genotypes)
-
-    for index, variant in enumerate(variants(vcf_file, show_progress)):
-        physical_pos = variant.position
-        if index >= max_variants or physical_pos >= map_length:
+    for index, site in enumerate(variants(vcf_file, show_progress)):
+        sample_data.add_site(site.position, site.alleles, site.genotypes, site.metadata)
+        if index == max_variants:
             break
-        genetic_pos = genetic_map.physical_to_genetic(variant.position)
-        physical_dist = physical_pos - last_physical_pos
-        genetic_dist = genetic_pos - last_genetic_pos
-        scaled_recomb_rate = 0
-        if genetic_dist > 0:
-            scaled_recomb_rate = physical_dist / genetic_dist
-        # Skipping recombination rate for now.
-        # recombination_rates.append(scaled_recomb_rate)
-        sample_data.add_variant(variant.position, ["0", "1"], variant.genotypes)
-
-        last_physical_pos = physical_pos
-        last_genetic_pos = genetic_pos
 
     sample_data.finalise()
     print("Wrote", output_file)
 
-
 def worker(t):
-    vcf, genetic_map, output, max_variants = t
+    vcf, output, max_variants = t
     print("Converting", vcf)
-    convert(vcf, genetic_map, output, max_variants)
+    convert(vcf, output, max_variants)
 
 
 def main():
@@ -136,8 +119,6 @@ def main():
         description="Script to convert VCF files into tsinfer input.")
     parser.add_argument(
         "vcf_pattern", help="The input VCF files file pattern.")
-    parser.add_argument(
-        "genetic_map_pattern", help="The input genetic maps in HapMap format file pattern")
     parser.add_argument(
         "output_file_pattern", help="The tsinfer output file pattern to write to.")
     parser.add_argument(
@@ -153,21 +134,17 @@ def main():
         "-p", "--progress", action="store_true")
 
     args = parser.parse_args()
+    # TODO Fix this up and make it an optional argument.
     chromosomes = list(range(args.start, args.stop + 1))
 
     # Build the file lists for the 22 autosomes.
     vcf_files = [args.vcf_pattern.format(j) for j in chromosomes]
-    genetic_map_files = [args.genetic_map_pattern.format(j) for j in chromosomes]
     output_files = [args.output_file_pattern.format(j) for j in chromosomes]
     max_variants = [args.max_variants for _ in chromosomes]
 
     for vcf_file in vcf_files:
         if not os.path.exists(vcf_file):
             raise ValueError("{} does not exist".format(vcf_file))
-
-    for genetic_map_file in genetic_map_files:
-        if not os.path.exists(genetic_map_file):
-            raise ValueError("{} does not exist".format(genetic_map_file))
 
 #     work = reversed(list(zip(
 #         vcf_files, genetic_map_files, output_files, max_variants)))
@@ -177,8 +154,7 @@ def main():
 #     #     worker(t)
 
     convert(
-        vcf_files[0], genetic_map_files[0], output_files[0], args.max_variants,
-        show_progress=args.progress)
+        vcf_files[0], output_files[0], args.max_variants, show_progress=args.progress)
 
 
 if __name__ == "__main__":
