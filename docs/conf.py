e15284dc77f195aebeb6b1c8de8927d2bdedd455
Yan Wong <yan.wong@bdi.ox.ac.uk>
2020-11-25 09:47:29 +0000

    Use autodoc_mock_imports
    I think this is how you are meant to do this now

--- docs/conf.py
+++ docs/conf.py
@@ -17,16 +17,8 @@ import os
 import sys
 
 import pkg_resources
-from unittest.mock import MagicMock
 
-
-class Mock(MagicMock):
-    @classmethod
-    def __getattr__(cls, name):
-        return MagicMock()
-
-
-MOCK_MODULES = [
+autodoc_mock_imports = [
     "numpy",
     "tskit",
     "_tsinfer",
@@ -38,7 +30,6 @@ MOCK_MODULES = [
     "daiquiri",
     "tqdm",
 ]
-sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
