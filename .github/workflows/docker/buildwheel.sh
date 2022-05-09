2e4c3eb76946947005b0a7d833ad207a6020b62e
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2022-05-09 10:33:08 +0100

    Fix setuptools_scm and disable windows wheels

--- .github/workflows/docker/buildwheel.sh
+++ .github/workflows/docker/buildwheel.sh
@@ -8,15 +8,19 @@ ARCH=`uname -p`
 echo "arch=$ARCH"
 #yum -y install gsl-devel #For msprime
 
-
+# We're running as root in the docker container so git commands issued by
+# setuptools_scm will fail without this:
+git config --global --add safe.directory /project
+# Fetch the full history as we'll be missing tags otherwise.
+git fetch --unshallow
 for V in "${PYTHON_VERSIONS[@]}"; do
     PYBIN=/opt/python/$V/bin
     rm -rf build/       # Avoid lib build by narrow Python is used by wide python
     # Instead of letting setup.py install a newer numpy we install it here
     # using the oldest supported version for ABI compatibility
     $PYBIN/pip install oldest-supported-numpy
-    $PYBIN/python setup.py build_ext --inplace
-    $PYBIN/python setup.py bdist_wheel
+    SETUPTOOLS_SCM_DEBUG=1 $PYBIN/python setup.py build_ext --inplace
+    SETUPTOOLS_SCM_DEBUG=1 $PYBIN/python setup.py bdist_wheel
 done
 
 cd dist
