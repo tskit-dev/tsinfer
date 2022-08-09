faf0880330f3293d105ccdab8c7a18bbfbc55c67
Ben Jeffery <ben.jeffery@gmail.com>
2022-08-09 16:14:13 +0100

    Use new doc theme

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ language = "en"
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+pygments_style = "monokai"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
@@ -108,7 +108,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "tskit-book-theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
