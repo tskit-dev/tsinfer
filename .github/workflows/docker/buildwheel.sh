de277cfca2c0acc3591fc131b0c9fa961446ff2f
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2022-10-13 10:54:30 +0100

    Update packaging

--- .github/workflows/docker/buildwheel.sh
+++ .github/workflows/docker/buildwheel.sh
@@ -6,7 +6,6 @@ set -e -x
 
 ARCH=`uname -p`
 echo "arch=$ARCH"
-#yum -y install gsl-devel #For msprime
 
 # We're running as root in the docker container so git commands issued by
 # setuptools_scm will fail without this:
@@ -14,13 +13,16 @@ git config --global --add safe.directory /project
 # Fetch the full history as we'll be missing tags otherwise.
 git fetch --unshallow
 for V in "${PYTHON_VERSIONS[@]}"; do
+    git reset --hard
+    git clean -fd
     PYBIN=/opt/python/$V/bin
     rm -rf build/       # Avoid lib build by narrow Python is used by wide python
     # Instead of letting setup.py install a newer numpy we install it here
     # using the oldest supported version for ABI compatibility
-    $PYBIN/pip install oldest-supported-numpy
-    SETUPTOOLS_SCM_DEBUG=1 $PYBIN/python setup.py build_ext --inplace
-    SETUPTOOLS_SCM_DEBUG=1 $PYBIN/python setup.py bdist_wheel
+    $PYBIN/python -m venv env
+    source env/bin/activate
+    $PYBIN/python -m pip install --upgrade build
+    SETUPTOOLS_SCM_DEBUG=1 $PYBIN/python -m build
 done
 
 cd dist
--- setup.py
+++ setup.py
@@ -1,26 +1,15 @@
 import os
 import platform
 
+import numpy
 from setuptools import Extension
 from setuptools import setup
-from setuptools.command.build_ext import build_ext as _build_ext
+
 
 IS_WINDOWS = platform.system() == "Windows"
 
-
-# Obscure magic required to allow numpy be used as an 'setup_requires'.
-class build_ext(_build_ext):
-    def finalize_options(self):
-        super().finalize_options()
-        # Prevent numpy from thinking it is still in its setup process:
-        __builtins__.__NUMPY_SETUP__ = False
-        import numpy
-
-        self.include_dirs.append(numpy.get_include())
-
-
 libdir = "lib"
-tskroot = os.path.join("lib", "subprojects", "tskit")
+tskroot = os.path.join(libdir, "subprojects", "tskit")
 tskdir = os.path.join(tskroot, "tskit")
 kasdir = os.path.join(tskroot, "subprojects", "kastore")
 includes = [libdir, tskroot, tskdir, kasdir]
@@ -53,59 +42,16 @@ if IS_WINDOWS:
 _tsinfer_module = Extension(
     "_tsinfer",
     sources=sources,
+    extra_compile_args=["-std=c99"],
+    libraries=libraries,
     # Enable asserts by default.
     undef_macros=["NDEBUG"],
-    extra_compile_args=["-std=c99"],
-    include_dirs=includes,
-    libraries=libraries,
+    include_dirs=includes + [numpy.get_include()],
 )
 
-with open("README.md") as f:
-    long_description = f.read()
-
 setup(
+    # The package name along with all the other metadata is specified in setup.cfg
+    # However, GitHub's dependency graph can't see the package unless we put this here.
     name="tsinfer",
-    description="Infer tree sequences from genetic variation data.",
-    long_description=long_description,
-    packages=["tsinfer"],
-    author="Jerome Kelleher",
-    author_email="jerome.kelleher@bdi.ox.ac.uk",
-    url="http://pypi.python.org/pypi/tsinfer",
-    python_requires=">=3.7",
-    entry_points={"console_scripts": ["tsinfer=tsinfer.__main__:main"]},
-    setup_requires=["setuptools_scm", "numpy"],
-    cmdclass={"build_ext": build_ext},
-    install_requires=[
-        "numpy",
-        "six",
-        "tqdm",
-        "humanize",
-        "daiquiri",
-        "tskit>=0.5.3",
-        "numcodecs>=0.6",
-        # issues 965 and 967 at zarr-python prevent usage of 2.11.0 and 2.11.1
-        "zarr>=2.2,!=2.11.0,!=2.11.1,!=2.11.2",
-        "lmdb",
-        "sortedcontainers",
-        "attrs>=19.2.0",
-    ],
     ext_modules=[_tsinfer_module],
-    keywords=[],
-    license="GNU GPLv3+",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3 :: Only",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
-        "Development Status :: 3 - Alpha",
-        "Environment :: Other Environment",
-        "Intended Audience :: Science/Research",
-        "Operating System :: POSIX",
-        "Topic :: Scientific/Engineering",
-        "Topic :: Scientific/Engineering :: Bio-Informatics",
-    ],
-    use_scm_version={"write_to": "tsinfer/_version.py"},
 )
