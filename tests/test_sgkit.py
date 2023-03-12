9e7b9fd9d4b58b8158228097128c07e0f48e15f0
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2023-03-12 23:28:32 +0000

    Complete testing and defaults for sgkit sampledata

--- tests/test_sgkit.py
+++ tests/test_sgkit.py
@@ -26,13 +26,30 @@ import msprime
 import numpy as np
 import pytest
 import sgkit
+import tskit
 import xarray as xr
 
 import tsinfer
 from tsinfer import formats
 
+EXAMPLE_SCHEMA = tskit.MetadataSchema(
+    {"codec": "json", "properties": {"foo": {"type": "integer"}}}
+)
 
-def make_ts_and_zarr(path):
+
+def add_array_to_dataset(name, array, zarr_path, dims=None):
+    ds = sgkit.load_dataset(zarr_path)
+    ds.update({name: xr.DataArray(data=array, dims=dims, name=name)})
+    sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {name}), zarr_path, mode="a")
+
+
+def add_attribute_to_dataset(name, contents, zarr_path):
+    ds = sgkit.load_dataset(zarr_path)
+    ds.attrs[name] = contents
+    sgkit.save_dataset(ds, zarr_path, mode="a")
+
+
+def make_ts_and_zarr(path, add_optional=False):
     import sgkit.io.vcf
 
     ts = msprime.sim_ancestry(
@@ -45,7 +62,31 @@ def make_ts_and_zarr(path):
     ts = msprime.sim_mutations(
         ts, rate=0.025, model=msprime.BinaryMutationModel(), random_seed=42
     )
+    tables = ts.dump_tables()
+    tables.metadata_schema = EXAMPLE_SCHEMA
+    sites_copy = tables.sites.copy()
+    tables.sites.clear()
+    tables.sites.metadata_schema = EXAMPLE_SCHEMA
+    for i, site in enumerate(sites_copy):
+        tables.sites.append(site.replace(metadata={"id_site": i}))
 
+    pops_copy = tables.populations.copy()
+    tables.populations.clear()
+    tables.populations.metadata_schema = EXAMPLE_SCHEMA
+    for i, pop in enumerate(pops_copy):
+        tables.populations.append(pop.replace(metadata={"id_pop": i}))
+
+    indiv_copy = tables.individuals.copy()
+    tables.individuals.clear()
+    tables.individuals.metadata_schema = EXAMPLE_SCHEMA
+    for i, ind in enumerate(indiv_copy):
+        tables.individuals.append(ind.replace(metadata={"id_indiv": i}))
+
+    ts = tables.tree_sequence()
+
+    # For simplicity, we would like go directly from the tree sequence to the sgkit
+    # dataset, but for testing it is desirable to have sgkit code write as much of the
+    # data as possible.
     with open(path / "data.vcf", "w") as f:
         ts.write_vcf(f)
     sgkit.io.vcf.vcf_to_zarr(
@@ -55,17 +96,114 @@ def make_ts_and_zarr(path):
         ploidy=3,
         max_alt_alleles=4,
     )
+
+    if add_optional:
+        add_attribute_to_dataset(
+            "sequence_length",
+            ts.sequence_length + 1337,
+            path / "data.zarr",
+        )
+        add_array_to_dataset(
+            "sites_metadata",
+            np.array(
+                [
+                    tables.sites.metadata_schema.encode_row(site.metadata)
+                    for site in ts.sites()
+                ]
+            ),
+            path / "data.zarr",
+            ["variants"],
+        )
+        add_array_to_dataset(
+            "sites_time",
+            np.arange(ts.num_sites) / ts.num_sites,
+            path / "data.zarr",
+            ["variants"],
+        )
+        add_attribute_to_dataset(
+            "sites_metadata_schema",
+            repr(tables.sites.metadata_schema),
+            path / "data.zarr",
+        )
+        add_attribute_to_dataset(
+            "metadata_schema",
+            repr(tables.metadata_schema),
+            path / "data.zarr",
+        )
+        add_array_to_dataset(
+            "provenances_timestamp",
+            ["2021-01-01T00:00:00", "2021-01-02T00:00:00"],
+            path / "data.zarr",
+        )
+        add_array_to_dataset(
+            "provenances_record",
+            ['{"foo": 1}', '{"foo": 2}'],
+            path / "data.zarr",
+        )
+        add_attribute_to_dataset(
+            "populations_metadata_schema",
+            repr(tables.populations.metadata_schema),
+            path / "data.zarr",
+        )
+        add_array_to_dataset(
+            "populations_metadata",
+            np.array(
+                [
+                    tables.populations.metadata_schema.encode_row(population.metadata)
+                    for population in ts.populations()
+                ]
+            ),
+            path / "data.zarr",
+            ["populations"],
+        )
+        add_array_to_dataset(
+            "individuals_time",
+            np.arange(ts.num_individuals, dtype=np.float32),
+            path / "data.zarr",
+            ["samples"],
+        )
+        add_array_to_dataset(
+            "individuals_metadata",
+            np.array(
+                [
+                    tables.individuals.metadata_schema.encode_row(individual.metadata)
+                    for individual in ts.individuals()
+                ]
+            ),
+            path / "data.zarr",
+            ["samples"],
+        )
+        add_array_to_dataset(
+            "individuals_location",
+            np.tile(np.array([["0", "1"]], dtype="float32"), (ts.num_individuals, 1)),
+            path / "data.zarr",
+            ["samples", "coordinates"],
+        )
+        add_array_to_dataset(
+            "individuals_population",
+            np.zeros(ts.num_individuals, dtype="int32"),
+            path / "data.zarr",
+            ["samples"],
+        )
+        add_array_to_dataset(
+            "individuals_flags",
+            np.random.RandomState(42).randint(
+                0, 2_000_000, ts.num_individuals, dtype="int32"
+            ),
+            path / "data.zarr",
+            ["samples"],
+        )
+        add_attribute_to_dataset(
+            "individuals_metadata_schema",
+            repr(tables.individuals.metadata_schema),
+            path / "data.zarr",
+        )
+
     return ts, path / "data.zarr"
 
 
-def add_array_to_dataset(name, array, zarr_path):
-    ds = sgkit.load_dataset(zarr_path)
-    ds.update({"variant_mask": xr.DataArray(data=array, dims=["variants"], name=name)})
-    sgkit.save_dataset(ds.drop_vars(set(ds.data_vars) - {name}), zarr_path, mode="a")
-
-
 @pytest.mark.skipif(sys.platform == "win32", reason="No cyvcf2 on windows")
-def test_sgkit_dataset(tmp_path):
+def test_sgkit_dataset_roundtrip(tmp_path):
     ts, zarr_path = make_ts_and_zarr(tmp_path)
     samples = tsinfer.SgkitSampleData(zarr_path)
     inf_ts = tsinfer.infer(samples)
@@ -75,12 +213,111 @@ def test_sgkit_dataset(tmp_path):
     assert inf_ts.num_edges > 200
 
 
+@pytest.mark.skipif(sys.platform == "win32", reason="No cyvcf2 on windows")
+def test_sgkit_dataset_accessors(tmp_path):
+    ts, zarr_path = make_ts_and_zarr(tmp_path, add_optional=True)
+    samples = tsinfer.SgkitSampleData(zarr_path)
+
+    assert samples.format_name == "tsinfer-sgkit-sample-data"
+    assert samples.format_version == (0, 1)
+    assert samples.finalised
+    assert samples.sequence_length == ts.sequence_length + 1337
+    assert samples.num_sites == ts.num_sites
+    assert samples.sites_metadata_schema == ts.tables.sites.metadata_schema.schema
+    assert samples.sites_metadata == [site.metadata for site in ts.sites()]
+    assert np.array_equal(samples.sites_time, np.arange(ts.num_sites) / ts.num_sites)
+    assert np.array_equal(samples.sites_position, ts.tables.sites.position)
+    assert np.array_equal(
+        samples.sites_alleles,
+        np.tile(np.array([["0", "1", "", "", ""]], dtype="O"), (ts.num_sites, 1)),
+    )
+    assert np.array_equal(samples.sites_mask, np.ones(ts.num_sites, dtype=bool))
+    assert np.array_equal(
+        samples.sites_ancestral_allele, np.zeros(ts.num_sites, dtype=np.int8)
+    )
+    assert np.array_equal(samples.sites_genotypes, ts.genotype_matrix())
+    assert np.array_equal(
+        samples.provenances_timestamp, ["2021-01-01T00:00:00", "2021-01-02T00:00:00"]
+    )
+    assert samples.provenances_record == [{"foo": 1}, {"foo": 2}]
+    assert samples.num_samples == ts.num_samples
+    assert np.array_equal(
+        samples.samples_individual, np.repeat(np.arange(ts.num_samples // 3), 3)
+    )
+    assert samples.metadata_schema == EXAMPLE_SCHEMA.schema
+    assert samples.metadata == ts.tables.metadata
+    assert (
+        samples.populations_metadata_schema
+        == ts.tables.populations.metadata_schema.schema
+    )
+    assert samples.populations_metadata == [pop.metadata for pop in ts.populations()]
+    assert samples.num_individuals == ts.num_individuals
+    assert np.array_equal(
+        samples.individuals_time, np.arange(ts.num_individuals, dtype=np.float32)
+    )
+    assert (
+        samples.individuals_metadata_schema
+        == ts.tables.individuals.metadata_schema.schema
+    )
+    assert samples.individuals_metadata == [ind.metadata for ind in ts.individuals()]
+    assert np.array_equal(
+        samples.individuals_location,
+        np.tile(np.array([["0", "1"]], dtype="float32"), (ts.num_individuals, 1)),
+    )
+    assert np.array_equal(
+        samples.individuals_population, np.zeros(ts.num_individuals, dtype="int32")
+    )
+    assert np.array_equal(
+        samples.individuals_flags,
+        np.random.RandomState(42).randint(
+            0, 2_000_000, ts.num_individuals, dtype="int32"
+        ),
+    )
+
+
+@pytest.mark.skipif(sys.platform == "win32", reason="No cyvcf2 on windows")
+def test_sgkit_accessors_defaults(tmp_path):
+    ts, zarr_path = make_ts_and_zarr(tmp_path)
+    samples = tsinfer.SgkitSampleData(zarr_path)
+
+    default_schema = tskit.MetadataSchema.permissive_json().schema
+    assert samples.sequence_length == ts.sequence_length
+    assert samples.sites_metadata_schema == default_schema
+    assert samples.sites_metadata == [{} for _ in range(ts.num_sites)]
+    for time in samples.sites_time:
+        assert tskit.is_unknown_time(time)
+    assert np.array_equal(samples.sites_mask, np.ones(ts.num_sites, dtype=bool))
+    assert np.array_equal(
+        samples.sites_ancestral_allele, np.zeros(ts.num_sites, dtype=np.int8)
+    )
+    assert np.array_equal(samples.provenances_timestamp, [])
+    assert np.array_equal(samples.provenances_record, [])
+    assert samples.metadata_schema == default_schema
+    assert samples.metadata == {}
+    assert samples.populations_metadata_schema == default_schema
+    assert samples.populations_metadata == []
+    assert samples.individuals_metadata_schema == default_schema
+    assert samples.individuals_metadata == [{} for _ in range(ts.num_individuals)]
+    for time in samples.individuals_time:
+        assert tskit.is_unknown_time(time)
+    assert np.array_equal(
+        samples.individuals_location, np.array([[]] * ts.num_individuals, dtype=float)
+    )
+    assert np.array_equal(
+        samples.individuals_population, np.full(ts.num_individuals, tskit.NULL)
+    )
+    assert np.array_equal(
+        samples.individuals_flags, np.zeros(ts.num_individuals, dtype=int)
+    )
+
+
 @pytest.mark.skipif(sys.platform == "win32", reason="File permission errors on Windows")
 class TestSgkitMask:
     @pytest.mark.parametrize("sites", [[1, 2, 3, 5, 9, 27], [0], []])
     def test_sgkit_variant_mask(self, tmp_path, sites):
         ts, zarr_path = make_ts_and_zarr(tmp_path)
         ds = sgkit.load_dataset(zarr_path)
+        print(ds)
         sites_mask = np.zeros_like(ds["variant_position"], dtype=bool)
         for i in sites:
             sites_mask[i] = True
@@ -102,6 +339,10 @@ class TestSgkitMask:
             ts.tables.sites.ancestral_state[sites_mask],
             inf_ts.tables.sites.ancestral_state,
         )
+        # TODO - site metadata needs merging not replacing
+        # assert [site.metadata for site in ts.sites() if site.id in sites] == [
+        #     site.metadata for site in inf_ts.sites()
+        # ]
 
     def test_sgkit_variant_bad_mask(self, tmp_path):
         ts, zarr_path = make_ts_and_zarr(tmp_path)
@@ -113,6 +354,29 @@ class TestSgkitMask:
             match="The variant_mask array contains values " "other than 0 or 1",
         ):
             tsinfer.SgkitSampleData(zarr_path)
+
+    def test_sgkit_variant_bad_mask_length(self, tmp_path):
+        ts, zarr_path = make_ts_and_zarr(tmp_path)
+        ds = sgkit.load_dataset(zarr_path)
+        sites_mask = np.ones(ds.sizes["variants"] + 1, dtype=int)
+        add_array_to_dataset("variant_mask", sites_mask, zarr_path)
+        with pytest.raises(
+            ValueError,
+            match="Mask must be the same length as the number of unmasked sites",
+        ):
+            tsinfer.SgkitSampleData(zarr_path)
+
+    def test_bad_mask_length_at_iterator(self, tmp_path):
+        ts, zarr_path = make_ts_and_zarr(tmp_path)
+        ds = sgkit.load_dataset(zarr_path)
+        sites_mask = np.ones(ds.sizes["variants"] + 1, dtype=int)
+        from tsinfer.formats import chunk_iterator
+
+        with pytest.raises(
+            ValueError, match="Mask must be the same length as the array"
+        ):
+            for _ in chunk_iterator(ds.variant_position, mask=sites_mask):
+                pass
 
 
 @pytest.mark.skipif(sys.platform == "win32", reason="File permission errors on Windows")
