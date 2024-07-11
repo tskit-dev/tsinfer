dc61b6a8f93b05734ef92ec7f64b61d355c4ca2b
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2024-07-11 12:37:00 +0100

    Upgrade actions, build with numpy2

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ import numpy
 from setuptools import Extension
 from setuptools import setup
 
-
 IS_WINDOWS = platform.system() == "Windows"
 
 libdir = "lib"
@@ -22,8 +21,6 @@ tsi_source_files = [
     "err.c",
     "avl.c",
 ]
-# We're not actually using very much of tskit at the moment, so
-# just build the stuff we need.
 tsk_source_files = ["core.c"]
 kas_source_files = ["kastore.c"]
 
@@ -34,24 +31,17 @@ sources = (
     + [os.path.join(kasdir, f) for f in kas_source_files]
 )
 
-libraries = []
-if IS_WINDOWS:
-    # Needed for generating UUIDs in tskit
-    libraries.append("Advapi32")
+libraries = ["Advapi32"] if IS_WINDOWS else []
 
 _tsinfer_module = Extension(
     "_tsinfer",
     sources=sources,
     extra_compile_args=["-std=c99"],
     libraries=libraries,
-    # Enable asserts by default.
     undef_macros=["NDEBUG"],
     include_dirs=includes + [numpy.get_include()],
 )
 
 setup(
-    # The package name along with all the other metadata is specified in setup.cfg
-    # However, GitHub's dependency graph can't see the package unless we put this here.
-    name="tsinfer",
     ext_modules=[_tsinfer_module],
 )
